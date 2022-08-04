10700a27e7a5c7db274ffe26ac4f72fdba8f026e
Phongnguyen163 <thanhphongnguyen99163@gmail.com>
2022-08-04 09:43:40 +0700

    chinh sua hien thi giao dien vi-phong-04/08

--- src/app/wallet/show-wallet/show-wallet.component.ts
+++ src/app/wallet/show-wallet/show-wallet.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import {WalletService} from "../../service/wallet.service";
 import {Wallet} from "../../model/wallet";
+import * as buffer from "buffer";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-show-wallet',
   templateUrl: './show-wallet.component.html',
@@ -11,7 +13,8 @@ export class ShowWalletComponent implements OnInit {
 
   wallets: Wallet[] = [];
 
-  constructor(private walletService: WalletService) { }
+  constructor(private walletService: WalletService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.showWallet();
@@ -20,7 +23,13 @@ export class ShowWalletComponent implements OnInit {
   showWallet() {
     this.walletService.findAll().subscribe(wallets => {
       this.wallets = wallets;
+      for (let i = 0; i < this.wallets.length; i++) {
+        if (this.wallets[i].status == 2) {
+          localStorage.setItem('ID_WALLET', JSON.stringify(this.wallets[i].id));
+        }
+      }
     })
+    this.router.navigate(['/wallet/' + localStorage.getItem('ID_WALLET')]).then();
   }
 
 
