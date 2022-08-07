273cdd605866bb33bd42303714e99e73a6ed7816
vulinh1704 <truongdangvulinh1704@gmail.com>
2022-08-07 13:04:28 +0700

    cập nhật home - 07/08 - linh

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {Options} from "@angular-slider/ngx-slider";
 import {TransactionService} from "../service/transaction.service";
 import {Transaction} from "../model/transaction";
 import {Chart, registerables} from "chart.js";
+import {ExportService} from "../service/export.service";
 
 Chart.register(...registerables);
 
@@ -12,17 +13,77 @@ Chart.register(...registerables);
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   isOpen: boolean = true;
-  transactions : Transaction[] = [];
+  transactions: Transaction[] = [];
+  arr: any[] = [20, 60, 20, 30, 40, 50, 48, 57];
 
-  constructor(private transactionService: TransactionService) {
+  //ranger
+  value: number = 0;
+  highValue: number = 0;
+  options: Options = {
+    floor: 0,
+    ceil: 1000,
+  };
+
+  constructor(private transactionService: TransactionService,
+              private exportService: ExportService) {
+
   }
-
-  arr: any[] = [20, 60, 20, 30 , 40, 50, 48,57];
 
   ngOnInit(): void {
     this.showTransaction();
+    this.chart3();
+    this.chart();
+    this.chart2();
+  }
+
+  isOpenHtml() {
+    // @ts-ignore
+    if (document.getElementById('detail').hidden) {
+      this.isOpen = false;
+    } else {
+      this.isOpen = true;
+    }
+  }
+
+  showTransaction() {
+    this.transactionService.findAll().subscribe(transactions => {
+      this.transactions = transactions;
+      console.log(transactions);
+      this.transactionList();
+    })
+  }
+
+  // chart
+  chart3() {
+    const ctx = document.getElementById('myChart3');
+    // @ts-ignore
+    const myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow',
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: this.arr,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            '#e66465'
+          ],
+          hoverOffset: 4
+        }]
+      },
+    });
+  }
+
+  chart() {
     const ctx = document.getElementById('myChart');
     // @ts-ignore
     const myChart = new Chart(ctx, {
@@ -48,82 +109,54 @@ export class HomeComponent implements OnInit {
         }]
       },
     });
+  }
 
-
+  chart2() {
     const ctx2 = document.getElementById('myChart2');
     // @ts-ignore
     const myChart2 = new Chart(ctx2, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        // datasets: [{
-        //   label: '# of Votes',
-        //   data: [12, 19, 3, 5, 2, 3],
-        // backgroundColor: [
-        //   'rgba(255, 99, 132, 0.2)',
-        //   'rgba(54, 162, 235, 0.2)',
-        //   'rgba(255, 206, 86, 0.2)',
-        //   'rgba(75, 192, 192, 0.2)',
-        //   'rgba(153, 102, 255, 0.2)',
-        //   'rgba(255, 159, 64, 0.2)'
-        // ],
-        // borderColor: [
-        //   'rgba(255, 99, 132, 1)',
-        //   'rgba(54, 162, 235, 1)',
-        //   'rgba(255, 206, 86, 1)',
-        //   'rgba(75, 192, 192, 1)',
-        //   'rgba(153, 102, 255, 1)',
-        //   'rgba(255, 159, 64, 1)'
-        // ],
-        // borderWidth: 1
-        datasets: [{
-          label: 'Data1',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: 'rgb(255, 99, 132)',
-          borderColor: 'rgb(255, 99, 132)',
-          borderWidth:1
-        },
-          {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
             label: 'Data1',
-            data: [6, 21, 3, 5, 2, 3],
-            backgroundColor: 'rgb(60,56,159)',
-            borderColor: 'rgb(138,30,53)',
-            borderWidth:1
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 1
+          },
+            {
+              label: 'Data1',
+              data: [6, 21, 3, 5, 2, 3],
+              backgroundColor: 'rgb(60,56,159)',
+              borderColor: 'rgb(138,30,53)',
+              borderWidth: 1
+            }
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
           }
-        ],
-  },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
         }
       }
-    }
+    );
   }
 
-);
-}
 
-value: number = 0;
-highValue: number = 0;
-options: Options = {
-  floor: 0,
-  ceil: 1000,
-};
-
-  isOpenHtml() {
-    // @ts-ignore
-    if (document.getElementById('detail').hidden) {
-      this.isOpen = false;
-    } else {
-      this.isOpen = true;
+  // exportFile
+  transactionFile: any[] = [];
+  transactionList(){
+    for (let i = 0; i < this.transactions.length; i++) {
+      this.transactionFile.push({'Số thứ tự': `${i + 1}`, 'Loại chi tiêu': `${this.transactions[i].category.status}`,
+        'Tổng tiền': `${this.transactions[i].totalSpent}`, 'Thời gian': `${this.transactions[i].time}`, 'Ghi chú': `${this.transactions[i].note}`})
     }
+    console.log('transaction',this.transactionFile)
   }
-
-  showTransaction() {
-    this.transactionService.findAll().subscribe(transactions => {
-      this.transactions = transactions;
-      console.log(transactions);
-    })
+  export() {
+    alert(1)
+    this.exportService.exportExcel(this.transactionFile, 'transactions');
   }
 }
