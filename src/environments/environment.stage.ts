91b9715b89117e96b524768195bcb7ed6ab71c0d
vulinh1704 <truongdangvulinh1704@gmail.com>
2022-08-10 22:50:48 +0700

    - add staging environment

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  apiUrl: process.env["API_URL"],
+  firebaseConfig: {
+    apiKey: "AIzaSyDuwg4uzVLBKXGznBoeoxQ25yjy60wNsyQ",
+    authDomain: "test-firebase-329d1.firebaseapp.com",
+    databaseURL: "https://test-firebase-329d1-default-rtdb.firebaseio.com",
+    projectId: "test-firebase-329d1",
+    storageBucket: "test-firebase-329d1.appspot.com",
+    messagingSenderId: "639022186555",
+    appId: "1:639022186555:web:5980976e2f28742a621c1e",
+    measurementId: "G-0Q7Y5D2GZP"
+  }
+};
